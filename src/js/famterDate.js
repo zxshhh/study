9edfa668c9b31e200c375c4e4180f5e2056c5e12
zhangxiaoshun <15010969174@163.com>
2019-02-18 18:02:16 +0800

    日历方法新加

---
+++ src/js/famterDate.js
@@ -0,0 +1,12 @@
+var date = new Date();
+var year = date.getFullYear();
+var month = date.getMonth() + 1;
+var day = date.getDate();
+if (month < 10) {
+    month = "0" + month;
+}
+if (day < 10) {
+    day = "0" + day;
+}
+var nowDate = year + "-" + month + "-" + day;
+console.log(nowDate);
